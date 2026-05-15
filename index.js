b6372ca886d3ad4175222af9fb75da7e6566b47d
krn2k6 <karannewbeginnings@gmail.com>
2026-05-16 00:04:40 +0530

    reset button added, logic fallacies solved by kanav, also added a input area to take

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
 let h1 = document.querySelector("h1");
+let input = document.querySelector("input")
+let reset = document.querySelector("button");
 
-count = 0;
-window.addEventListener("keyup", (dets)=>{
-    // console.log(dets);
-    if(dets.key !== "AltGraph" && dets.key !== "Control" && dets.key !== "Shift" && dets.key !== "Enter" && dets.key !== "Tab" && dets.key !== "Backspace" && dets.key !== "Alt" && dets.key !== "CapsLock" && dets.key !== "ArrowLeft" && dets.key !== "ArrowRight" && dets.key !== "ArrowUp" && dets.key !== "ArrowDown")
-        {
-        count++;
-        h1.textContent = `Character Counter: ${count}`;
-    }
-})
+
+let counter = 0;
+h1.textContent = `Character Counter: ${counter}`;
+input.addEventListener("keydown", (dets)=>{
+    // console.log(dets)
+    // if(true){
+
+    //     counter++;
+    //     if(counter>=0){
+            
+    //         h1.textContent = `Character Counter: ${counter}`;
+    //     }
+        
+    //     if(dets.key === "Backspace"){
+        //         counter--;
+        //         h1.textContent = `Character Counter: ${counter}`;
+        //     }
+        // }
+        if(dets.key !== "AltGraph" && dets.key !== "Control" && dets.key !== "Shift" && dets.key !== "Enter" && dets.key !== "Tab" && dets.key !== "Alt" && dets.key !== "CapsLock" && dets.key !== "ArrowLeft" && dets.key !== "ArrowRight" && dets.key !== "ArrowUp" && dets.key !== "ArrowDown"){
+
+            if(dets.key === "Backspace"){
+                counter--;
+            }
+            else{
+                counter++;
+            }
+            if(counter<0){
+                counter = 0;
+            }
+            h1.textContent = `Character Counter: ${counter}`;
+        }
+        
+    })
+    
+    reset.addEventListener("click", ()=>{
+        counter = 0;
+        h1.textContent = `Character Counter: ${counter}`;
+        input.value = "";
+    })
+    
+    
+// count = 0;
+// window.addEventListener("keyup", (dets)=>{
+//     // console.log(dets);
+//         {
+//         count++;
+//         h1.textContent = `Character Counter: ${count}`;
+//     }
+//     if(dets.key === "Backspace"){
+//         count--;
+//         if(count < 0){
+//             count = 0;
+//         }
+//         h1.textContent = `Character Counter: ${count}`;
+//     }
+// })
+
 
 // messy code i know but we'll optimize it later, and dont ask me to add some cool css im bored of that shit...
 
