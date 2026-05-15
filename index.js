700fe77e1775b4966ba11d959f71ede522476aab
krn2k6 <karannewbeginnings@gmail.com>
2026-05-16 00:57:25 +0530

    idk

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ let input = document.querySelector("input")
 let reset = document.querySelector("button");
 
 
-let counter = 0;
-h1.textContent = `Character Counter: ${counter}`;
-input.addEventListener("keydown", (dets)=>{
+// let counter = 0;
+// h1.textContent = `Character Counter: ${counter}`;
+// input.addEventListener("keydown", ()=>{
+
+input.addEventListener("input", ()=>{
+
+
     // console.log(dets)
     // if(true){
 
@@ -20,29 +24,62 @@ input.addEventListener("keydown", (dets)=>{
         //         h1.textContent = `Character Counter: ${counter}`;
         //     }
         // }
-        if(dets.key !== "AltGraph" && dets.key !== "Control" && dets.key !== "Shift" && dets.key !== "Enter" && dets.key !== "Tab" && dets.key !== "Alt" && dets.key !== "CapsLock" && dets.key !== "ArrowLeft" && dets.key !== "ArrowRight" && dets.key !== "ArrowUp" && dets.key !== "ArrowDown"){
 
-            if(dets.key === "Backspace"){
-                counter--;
-            }
-            else{
-                counter++;
-            }
-            if(counter<0){
-                counter = 0;
-            }
-            h1.textContent = `Character Counter: ${counter}`;
-        }
+
+
+
+
+
+
+    //     if(dets.key !== "AltGraph" && dets.key !== "Shift" && dets.key !== "Enter" && dets.key !== "Tab" && dets.key !== "Alt" && dets.key !== "CapsLock" && dets.key !== "ArrowLeft" && dets.key !== "ArrowRight" && dets.key !== "ArrowUp" && dets.key !== "ArrowDown"){
+
+    //         if(dets.key === "Backspace"){
+    //             counter--;
+    //         }
+    //         else{
+    //             counter++;
+    //         }
+    //         if(counter<0){
+    //             counter = 0;
+    //         }
+    //         if(dets.key === "Control" && dets.key === "Backspace"){
+    //             counter = 0;
+    //         }
+    //         h1.textContent = `Character Counter: ${counter}`;
+    //     }
         
-    })
+    //     h1.textContent = `Character Counter: ${counter}`;
+        
+        
+    // })
     
-    reset.addEventListener("click", ()=>{
-        counter = 0;
-        h1.textContent = `Character Counter: ${counter}`;
-        input.value = "";
-    })
-    
-    
+    // reset.addEventListener("click", ()=>{
+    //     counter = 0;
+    //     h1.textContent = `Character Counter: ${counter}`;
+    //     input.value = "";
+    // })
+
+
+
+
+
+    h1.textContent = `Character Counter: ${input.value.length}`
+
+
+})
+
+
+reset.addEventListener("click", ()=>{
+    input.value = "";
+    h1.textContent = `Character Counter: ${input.value.length}`;
+})
+  
+
+
+
+
+
+
 // count = 0;
 // window.addEventListener("keyup", (dets)=>{
 //     // console.log(dets);
@@ -58,6 +95,12 @@ input.addEventListener("keydown", (dets)=>{
 //         h1.textContent = `Character Counter: ${count}`;
 //     }
 // })
+
+
+
+
+
+
 
 
 // messy code i know but we'll optimize it later, and dont ask me to add some cool css im bored of that shit...
